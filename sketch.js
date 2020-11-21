df6d00afe9eedbb1f2cf5910cd53f29c982180f1
manisha3ch <73401128+manisha3ch@users.noreply.github.com>
2020-11-21 08:06:33 +0530

    restitution added for package

--- sketch.js
+++ sketch.js
@@ -47,7 +47,6 @@ function setup() {
 	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
-	//groundSprite.shapeColor=color(255)
 	groundSprite.visible=false;
 
 
@@ -58,26 +57,29 @@ function setup() {
  	boxPosition=canvasWidth/2-100
 	boxY=canvasHeight-90;
 
+	// Left side wall of the drop box
  	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
  	boxleftSprite.shapeColor=color(255,0,0);
 
  	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxLeftBody);
 
+	 // base of the drop box
  	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
  	boxBase.shapeColor=color(255,0,0);
 
  	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
- 	World.add(world, boxBottomBody);
 
- 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
+	// Right side wall of the drop box
+	boxRightSprite=createSprite(boxPosition+200 , boxY, 20,100);
+	boxRightSprite.shapeColor=color(255,0,0);
 
  	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxRightBody);
+
+	/// In one go add all the box walls of our world to the Matter.World
+	 World.add(world,[boxLeftBody,boxBottomBody,boxRightBody])   
 
 	for (var i=0;i<3;i++){
-		packageBody[i] = Bodies.circle(width/2 , 200 , 5 , { isStatic:true});
+		packageBody[i] = Bodies.circle(width/2 , 200 , 5 , { isStatic:true,restitution:0.4});
 		World.add(world, packageBody[i]);	
 	}
 
